b839c3415dc34636ea0ca8e49a46c61395b8796c
devkim404 <120579845+devkim404@users.noreply.github.com>
2023-03-02 03:19:24 +0900

    메인 컴포넌트 생성, Route 설정

--- booklist/src/App.tsx
+++ booklist/src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ROUTE_PATH } from './mobile/routes/routes';
+import Exhibition from './mobile/components/exhibition/Exhibition';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTE_PATH.EXHIBITION} element={<Exhibition />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
